3eb06c1b32599ba3ec41609d61dc8803d931b6dc
Charlie <cmoog@fwparker.org>
2017-03-05 10:08:08 +0000

    //

--- recursion_problem_set.py
+++ recursion_problem_set.py
@@ -28,16 +28,16 @@ def f(balance, index):
 f(5000, 1)
 
 #3.  You have $10000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY).  If you make the minimum payment of $100 per month, how many months will it take to pay it off?  Solve recursively.
-
+ '''Did you mean to write 1,000 as the starting balance - it doesn't seem that is is ever payed off with 10,000'''
 def h(balance, index, payed):
-    balance += balance * 0.016666666666666666666
     balance -= 100
+    balance += balance * 0.016666666666666666666
     if balance <= 0:
         payed = True
         print("It took", index, "months")
     if not payed and index < 1000:
         h(balance, index + 1, False)
-h(10000, 1, False)
+h(1000, 1, False)
 
 
 #4  Pyramid of Cubes - (10pts) If you stack boxes in a pyramid, the top row would have 1 box, the second row would have two, the third row would have 3 and so on.  Make a recursive function which calculates the TOTAL NUMBER OF BOXES for a pyramid of boxes n high.  For instance, a pyramid that is 3 high would have a total of 6 boxes.  A pyramid 4 high would have 10.
