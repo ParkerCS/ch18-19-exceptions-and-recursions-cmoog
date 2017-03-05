ffd4b9e84583dee41d438a2296d4ede38725a377
Charlie <cmoog@fwparker.org>
2017-03-05 15:38:00 +0000

    //

--- recursion_lab.py
+++ recursion_lab.py
@@ -9,3 +9,24 @@ to change the appearance and behavior.
 Give your fractal a depth of at least 5.  Ensure the fractal is contained on the screen (at whatever size you set it).  Have fun.
 (35pts)
 '''
+
+import turtle
+
+def fractal(x, y, distance, index):
+    my_turtle = turtle.Turtle()
+    if index == 0:
+        my_turtle.showturtle()
+        my_screen = turtle.Screen()
+        my_screen.bgcolor('blue')
+        index += 1
+    my_turtle.up()
+    my_turtle.goto(x, y)
+    my_turtle.down()
+
+    my_turtle.setheading(90)
+    my_turtle.forward(distance)
+
+    fractal(my_turtle.xcor(), my_turtle.ycor(), distance/10, index)
+
+fractal(100, 100, 100, 0)
+
